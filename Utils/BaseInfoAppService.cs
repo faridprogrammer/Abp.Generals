6a5c480306f8f77fce7032dbeb731f8e1511e66e
Farid Bekran <farid@appson.ir>
2019-09-02 11:23:07 +0430

    base info added

---
+++ Utils/BaseInfoAppService.cs
@@ -0,0 +1,44 @@
+﻿using Abp;
+using Abp.Application.Services;
+using Abp.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abp.Utils.BaseInfo
+{
+    public interface IBaseInfoAppService : IApplicationService {
+        IEnumerable<KeyValuePair<int, string>> EnumItems(string enumType);
+    }
+    [AbpAuthorize]
+    public class BaseInfoAppService : ApplicationService, IBaseInfoAppService
+    {
+        public BaseInfoAppService()
+        {
+
+        }
+        public static Type GetEnumType(string enumName)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var type = assembly.GetType(enumName);
+                if (type == null)
+                    continue;
+                if (type.IsEnum)
+                    return type;
+            }
+            return null;
+        }
+
+        public IEnumerable<KeyValuePair<int, string>> EnumItems(string enumType)
+        {
+            var values = Enum.GetValues(GetEnumType(enumType));
+            var res = new List<KeyValuePair<int, string>>();
+            foreach (Enum item in values)
+            {
+                res.Add(new KeyValuePair<int, string>(Convert.ToInt32(item), EnumHelper.GetDescription(item)));
+            }
+            return res;
+        }
+    }
+}
